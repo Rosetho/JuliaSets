f9f2f8f5064dbe3a9968aa7ebb353a94404e1dff
Tom Rose <Rosetho@kean.edu>
2019-06-15 12:31:07 -0400

    unfinsihed, map works but display is wrong

--- Mandel/mandel.sh
+++ Mandel/mandel.sh
@@ -5,31 +5,37 @@ j=0
 
 #i real, j is imaginary
 #z^2 +c where z=0 and c varies
-while [ $i -lt 20  ] do 
-	while [ $j -lt 20 ] do
-		zre=0
-		zim=0
-		
-		#map cre from i
-		#map cim from j
-		it=0
-		while [ $it < 100 ] && [ $((zre*zre + zim*zim)) -lt 2 ] do
-			$zretmp=$((zre*zre - zim*zim+ $cre))
-			$zim=$((2*zre*zim + $cim))
-			$zre=$zretmp
+#foreach pixel: map to c, iter loop, 
+while [ $i -lt 30  ] 
+	do 
+		while [ $j -lt 30 ] 
+			do
+				zre=0
+				zim=0
+				#[0,10]->[-2,2] mapping
+				cre=$((i*4/19-2))
+				cim=$((j*4/19-2))
 
-
+				it=0
+				
+				while [ $it -lt 100 ] && [ $((zre*zre + zim*zim)) -le 4 ] 
+					do
+						zretmp=$((zre*zre - zim*zim+  $cre))
+						zim=$((2*zre*zim + $cim))
+						zre=$zretmp
+						((it++))
+					done
+				
+				if [ $it -eq 100 ]
+					then
+						printf "* "
+					else 	
+						printf "$it "
+				fi
 		
-		done
-		if [ $j == $i ]
-			then
-				printf " "
-			else 	
-				printf "* "
-		fi
-		((j++))
+				((j++))
+			done
+		((i++))
+		j=0
+		printf "\n"
 	done
-	((i++))
-	j=0
-	printf "\n"
-done
